f7cc76167056f458e6d49ad06a9b07929738a668
Minsung <sns2831@gmail.com>
2020-12-18 23:20:24 +0900

    feat(baseball): 낫싱 계산 로직 구현

---
+++ src/main/java/baseball/model/GameResult.java
@@ -0,0 +1,48 @@
+package baseball.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameResult {
+    private static final String BALL = "%d볼";
+    private static final String STRIKE = "%d스트라이크";
+    private static final String NOTHING = "낫싱";
+    private static final String DELIMITER = " ";
+
+    private List<String> resultMessage;
+    private int strike;
+    private int ball;
+
+    public GameResult(int strike, int ball) {
+        this.strike = strike;
+        this.ball = ball;
+        resultMessage = new ArrayList<>();
+    }
+
+    private void strike() {
+        if (strike == 0) {
+            return;
+        }
+
+        resultMessage.add(String.format(STRIKE, strike));
+    }
+
+    private void ball() {
+        if (ball == 0) {
+            return;
+        }
+
+        resultMessage.add(String.format(BALL, ball));
+    }
+
+    @Override
+    public String toString() {
+        ball();
+        strike();
+        if(resultMessage.isEmpty()) {
+            return NOTHING;
+        }
+
+        return String.join(DELIMITER, resultMessage);
+    }
+}
--- src/main/java/baseball/model/service/BaseballGameService.java
+++ src/main/java/baseball/model/service/BaseballGameService.java
@@ -1,9 +1,13 @@
 package baseball.model.service;
 
+import baseball.model.GameResult;
 import baseball.model.baseball.Baseballs;
 
 public class BaseballGameService {
-    public String match(Baseballs b1, Baseballs b2) {
-        return null;
+    public GameResult match(Baseballs b1, Baseballs b2) {
+        int strike = b1.calculateStrike(b2);
+        int ball = b1.calculateBall(b2);
+
+        return new GameResult(strike, ball);
     }
 }
